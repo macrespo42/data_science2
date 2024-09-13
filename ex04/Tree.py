9d275467df59efbb9cf5e56f16bdb51061c2e53b
macrespo42 <macrespo@student.42.fr>
2024-09-13 18:46:19 +0200

    feat: improve model f1_score

--- ex04/Tree.py
+++ ex04/Tree.py
@@ -2,47 +2,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import tree
 from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 
 
+def preprocess_data(training, validation):
+    features = ["Push", "Lightsaber", "Friendship", "Attunement"]
+
+    X_train = training[features]
+    X_test = validation[features]
+
+    scaler = StandardScaler()
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+
+    y_train = [0 if x == "Jedi" else 1 for x in training["knight"]]
+    y_test = [0 if x == "Jedi" else 1 for x in validation["knight"]]
+
+    return X_train, X_test, y_train, y_test
+
+
 def main() -> None:
-    train = pd.read_csv("Train_knight.csv")
-    train["knight"] = [1 if x == "Jedi" else 0 for x in train["knight"]]
-    y = train["knight"]
-    X = train.drop(
-        [
-            "knight",
-            "Sensitivity",
-            "Recovery",
-            "Strength",
-            "Stims",
-            "Midi-chlorien",
-            "Agility",
-            "Evade",
-            "Grasping",
-            "Burst",
-            "Awareness",
-            "Empowered",
-            "Slash",
-            "Combo",
-            "Dexterity",
-            "Power",
-            "Delay",
-            "Pull",
-            "Reactivity",
-            "Blocking",
-            "Repulse",
-            "Sprint",
-            "Deflection",
-            "Mass",
-            "Survival",
-        ],
-        axis=1,
-    )
+    training = pd.read_csv("ex04/Training_data.csv")
+    validation = pd.read_csv("ex04/Validation_data.csv")
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
+    X_train, X_test, y_train, y_test = preprocess_data(training, validation)
     dtc = DecisionTreeClassifier(random_state=42)
     model = dtc.fit(X_train, y_train)
 
@@ -51,21 +36,21 @@ def main() -> None:
 
     print(f"f1_score: {f1 * 100}%")
 
-    x_test = pd.read_csv("Test_knight.csv")
-    y_pred = model.predict(x_test[["Push", "Lightsaber", "Friendship", "Attunement"]])
-
-    with open("preds.txt", "w+") as f:
-        for pred in y_pred:
-            if pred == 1:
-                f.write("Jedi\n")
-            else:
-                f.write("Sith\n")
-
-    fig = plt.figure(figsize=(25, 20))
-    _ = tree.plot_tree(
-        dtc, feature_names=X.columns, class_names=["Jedi", "Sith"], filled=True
-    )
-    fig.savefig("./tree.png")
+    # x_test = pd.read_csv("Test_knight.csv")
+    # y_pred = model.predict(x_test[["Push", "Lightsaber", "Friendship", "Attunement"]])
+    #
+    # with open("preds.txt", "w+") as f:
+    #     for pred in y_pred:
+    #         if pred == 1:
+    #             f.write("Jedi\n")
+    #         else:
+    #             f.write("Sith\n")
+    #
+    # fig = plt.figure(figsize=(25, 20))
+    # _ = tree.plot_tree(
+    #     dtc, feature_names=X.columns, class_names=["Jedi", "Sith"], filled=True
+    # )
+    # fig.savefig("./tree.png")
 
 
 if __name__ == "__main__":
