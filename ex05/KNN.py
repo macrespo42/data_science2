ac7430f4ae16386f038f7918f66f53bc2f782fc8
macrespo42 <macrespo@student.42.fr>
2024-09-14 16:32:38 +0200

    feat: KNN up to 94% f1 score

--- ex05/KNN.py
+++ ex05/KNN.py
@@ -1,17 +1,45 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import f1_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+
+def preprocess_data(training, validation):
+    features = ["Push", "Lightsaber", "Friendship", "Attunement"]
+
+    X_train = training[features]
+    X_test = validation[features]
+
+    scaler = StandardScaler()
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+
+    y_train = [0 if x == "Jedi" else 1 for x in training["knight"]]
+    y_test = [0 if x == "Jedi" else 1 for x in validation["knight"]]
+
+    return X_train, X_test, y_train, y_test
 
 
 def main() -> None:
+    training = pd.read_csv("ex04/Training_data.csv")
+    validation = pd.read_csv("ex04/Validation_data.csv")
+
+    X_train, X_test, y_train, y_test = preprocess_data(training, validation)
+
     df = pd.read_csv("Train_knight.csv")
+    knn = KNeighborsClassifier(n_neighbors=3)
+
+    model = knn.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    f1 = f1_score(y_test, y_pred)
+    print(f"f1_score: {f1 * 100}%")
 
     features = ["Push", "Lightsaber", "Friendship", "Attunement"]
     X_train = df[features]
     y_train = [0 if x == "Jedi" else 1 for x in df["knight"]]
 
-    knn = KNeighborsClassifier(n_neighbors=3)
     model = knn.fit(X_train, y_train)
 
     test = pd.read_csv("Test_knight.csv")
