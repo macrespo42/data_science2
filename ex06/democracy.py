b454f86d62744622a095e21ef82dd7f5d2b8fe52
macrespo42 <macrespo@student.42.fr>
2024-10-07 14:36:10 +0200

    feat: ex06 interactive

--- ex06/democracy.py
+++ ex06/democracy.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 from sklearn import model_selection
 from sklearn.ensemble import VotingClassifier
@@ -6,36 +7,32 @@ from sklearn.metrics import f1_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 
-def preprocess_data(training, validation):
-    features = ["Push", "Lightsaber", "Friendship", "Attunement"]
+def main() -> None:
+    if len(sys.argv) != 3:
+        print("provide 2 arguments: truth and predictions files")
+        sys.exit(1)
+    training = sys.argv[1]
+    validation = sys.argv[2]
 
-    X_train = training[features]
-    X_test = validation[features]
+    try:
+        training_df = pd.read_csv(training)
+        validation_df = pd.read_csv(validation)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
+
+    X = training_df.drop("knight", axis=1)
+    y = training_df["knight"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
     scaler = StandardScaler()
 
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.fit_transform(X_test)
-
-    y_train = [0 if x == "Jedi" else 1 for x in training["knight"]]
-    y_test = [0 if x == "Jedi" else 1 for x in validation["knight"]]
-
-    return X_train, X_test, y_train, y_test
-
-
-def main() -> None:
-    training = None
-    validation = None
-    try:
-        training = pd.read_csv("datasets/Training_data.csv")
-        validation = pd.read_csv("datasets/Validation_data.csv")
-    except Exception:
-        print("Please run the script from the root directory")
-        exit(1)
-
-    X_train, X_test, y_train, y_test = preprocess_data(training, validation)
 
     clf1 = LogisticRegression(random_state=42)
     clf2 = DecisionTreeClassifier(random_state=42)
@@ -54,20 +51,14 @@ def main() -> None:
         estimators=[(labels[0], clf1), (labels[1], clf2), (labels[2], clf3)],
         voting="hard",
     )
-    # D'abord, on doit entraîner le classifieur avec fit()
     voting_clf_hard.fit(X_train, y_train)
-    # Ensuite on peut faire les prédictions sur X_test
     y_pred = voting_clf_hard.predict(X_test)
-    # Calculer et afficher le F1-score
-    f1 = f1_score(y_test, y_pred)
+    f1 = f1_score(y_test.values, y_pred, pos_label='Sith')
     print(f"F1-score: {f1:.4f}")
 
     with open("Voting.txt", "w+") as f:
         for pred in y_pred:
-            if pred == 0:
-                f.write("Jedi\n")
-            else:
-                f.write("Sith\n")
+            f.write(f"{pred}\n")
 
 
 if __name__ == "__main__":
